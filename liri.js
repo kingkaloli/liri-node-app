ee4fab0e6083f9441be586ce8dcedac68fbe34a4
Kaloli <rvellark@gmail.com>
2019-02-03 13:48:29 -0700

    worked on spotify app and got this working

--- liri.js
+++ liri.js
@@ -1,7 +1,34 @@
-js
-require("dotenv").config();
-var spotify = new Spotify(keys.spotify);
 
+require("dotenv").config();
+var keys = require("./keys");
+
+
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify(keys.spotify);
+ 
+spotify.search({ type: 'track', query: 'The Sign Ace of Base.' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+
+var find = data.tracks.items[0];
+var songname = find.name;
+var preview= find.preview_url;
+var album = find.album.name;
+var artists = find.artists;
+
+for (var i = 0; i < artists.length; i++) {
+    console.log(artists[i].name);
+}
+console.log(songname);
+console.log(preview);
+console.log(album);
+
+
+
+});
 
 
 
