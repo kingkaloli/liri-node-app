a7bd567db41db17c1a610768e51de2f8d14c7260
Kaloli <rvellark@gmail.com>
2019-02-03 15:08:17 -0700

    update with query speaking to app

--- liri.js
+++ liri.js
@@ -2,35 +2,59 @@
 require("dotenv").config();
 var keys = require("./keys");
 var axios = require("axios");
-
-
 var Spotify = require('node-spotify-api');
+var read = process.argv[2];
+var query ="";
+for (var i= 3;i < process.argv.length; i++) {
+    query = query + process.argv[i] + " "
 
-var spotify = new Spotify(keys.spotify);
- 
-spotify.search({ type: 'track', query: 'The Sign Ace of Base.' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-
-var find = data.tracks.items[0];
-var songname = find.name;
-var preview= find.preview_url;
-var album = find.album.name;
-var artists = find.artists;
-
-for (var i = 0; i < artists.length; i++) {
-    console.log(artists[i].name);
 }
-console.log(songname);
-console.log(preview);
-console.log(album);
+console.log(read);
+console.log(query);
+
+if (read === "spotify-this-song") {
+spotifySearch(query)
+
+} else if (read === "movie-this") {
+    omdbSearch()
+}
 
 
+ function spotifySearch(songName) {
+     var query =  'The Sign Ace of Base.';
+     if (songName !== "") {
+     query = songName 
+        
+     }
 
-});
+    var spotify = new Spotify(keys.spotify);
+    
+    spotify.search({ type: 'track', query:query }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+     
+    
+    var find = data.tracks.items[0];
+    var songname = find.name;
+    var preview= find.preview_url;
+    var album = find.album.name;
+    var artists = find.artists;
+    
+    for (var i = 0; i < artists.length; i++) {
+        console.log(artists[i].name);
+    }
+    console.log(songname);
+    console.log(preview);
+    console.log(album);
+    
+    
+    
+    });
 
+ }
+
+ function omdbSearch() {
 // http://www.omdbapi.com/?t=mr+nobody&apikey=[yourkey] //
 
 axios.get('http://www.omdbapi.com/', {
@@ -53,4 +77,4 @@ axios.get('http://www.omdbapi.com/', {
   .catch(function (error) {
     console.log(error);
   });
-
+ }
